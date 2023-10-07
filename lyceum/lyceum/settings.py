26c8bd588b826a43634796f7e1afd5f2e5fd67b0
JaydenPears <vladimir120905@gmail.com>
2023-10-07 15:41:57 +0300

    Update env & requirements

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from decouple import config
+import os
+import sys
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+
+env = environ.Env()
+ENV_FILE_PATH = os.environ.get('ENV_FILE_PATH', '.env.example')
+env.read_env(ENV_FILE_PATH)
+
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
